f616dc334431cea40e6f0fabf0ff6844db986ee1
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-07-11 07:51:26 -0500

    refactor: use `TimeInterval`

--- hyprshade/config.py
+++ hyprshade/config.py
@@ -9,6 +9,8 @@ from more_itertools import nth, partition
 
 from hyprshade.utils import hyprshade_config_home, is_time_between
 
+TimeInterval = tuple[time, time]
+
 
 class ShadeDict(TypedDict):
     name: str
@@ -66,7 +68,7 @@ class Schedule:
         self.default_shade_name = default_shade and default_shade["name"]
 
     def find_shade(self, t: time) -> str | None:
-        for entry_name, start_time, end_time in self._resolved_entries():
+        for entry_name, (start_time, end_time) in self._resolved_entries():
             if is_time_between(t, start_time, end_time):
                 return entry_name
 
@@ -78,11 +80,11 @@ class Schedule:
             if entry.end_time is not None:
                 yield entry.end_time
 
-    def _resolved_entries(self):
+    def _resolved_entries(self) -> Iterable[tuple[str, TimeInterval]]:
         for entry, next_entry in pairwise(chain(self.entries, [self.entries[0]])):
             start_time = entry.start_time
             end_time = entry.end_time or next_entry.start_time
-            yield entry.name, start_time, end_time
+            yield entry.name, (start_time, end_time)
 
 
 class Config:
