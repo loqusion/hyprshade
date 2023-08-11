c6b971732761ee71f088e284afcfc45b1fb886c7
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-11 14:57:31 -0500

    feat: make `validate_optional_param()` generic

--- hyprshade/utils.py
+++ hyprshade/utils.py
@@ -1,6 +1,7 @@
 import os
 from datetime import time
 from os import path
+from typing import TypeVar
 
 import click
 
@@ -33,13 +34,16 @@ def is_time_between(time_: time, start_time: time, end_time: time) -> bool:
     return start_time <= time_ <= end_time
 
 
+T = TypeVar("T", str, int, float, bool, click.ParamType)
+
+
 def validate_optional_param(
-    ctx: click.Context, param: click.Argument, value: tuple[str, ...]
-) -> str | None:
+    ctx: click.Context, param: click.Argument, value: tuple[T, ...]
+) -> T | None:
     if extra_args := value[1:]:
         s = "s" if len(extra_args) > 1 else ""
         raise click.UsageError(
-            f"Got unexpected extra argument{s} ({' '.join(extra_args)})"
+            f"Got unexpected extra argument{s} ({' '.join(map(str, extra_args))})"
         )
 
     return None if len(value) == 0 else value[0]
