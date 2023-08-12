c75aadcfe2bd95f5df15571c7407b5222691a83c
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-12 16:31:24 -0500

    chore: remove unused function

--- hyprshade/helpers.py
+++ hyprshade/helpers.py
@@ -1,27 +1,7 @@
-from glob import iglob
 from os import path
 from typing import Literal
 
-from more_itertools import first
-
 from hyprshade.utils import systemd_user_config_home
-
-from .constants import SHADER_DIRS
-
-
-def resolve_shader_path(shader_name_or_path: str) -> str:
-    if path.isfile(shader_name_or_path):
-        return shader_name_or_path
-
-    for shaders_dir in SHADER_DIRS:
-        shader_path = first(
-            iglob(f"{shader_name_or_path}*", root_dir=shaders_dir), None
-        )
-        if shader_path is not None:
-            return path.join(shaders_dir, shader_path)
-
-    raise FileNotFoundError(f"Shader {shader_name_or_path} does not exist")
-
 
 SystemdUnitType = Literal["service", "timer"]
 
