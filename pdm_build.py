1e4de526b50da19982f4549b15c9f22d849f32b3
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-03 22:03:43 -0500

    build(completion): attempt completion fix

--- pdm_build.py
+++ pdm_build.py
@@ -16,9 +16,13 @@ FILE_NAMES: Final = {
 }
 
 
-def generate_completions(shell: str, target_dir: Path):
+def generate_completions(shell: str, target_dir: Path, root_dir: Path):
+    completion_script = root_dir / "completion.sh"
     pipe = subprocess.run(
-        ["bash", "completion.sh", shell], capture_output=True, text=True, check=True
+        ["bash", str(completion_script), shell],
+        capture_output=True,
+        text=True,
+        check=True,
     )
 
     file_name = FILE_NAMES.get(shell, None)
@@ -35,4 +39,4 @@ def pdm_build_initialize(context: Context):
     completions_dir.mkdir(parents=True, exist_ok=True)
 
     for shell in ["bash", "zsh", "fish"]:
-        generate_completions(shell, completions_dir)
+        generate_completions(shell, completions_dir, context.root)
