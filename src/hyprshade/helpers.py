d48ed39c2efda972c419d1e4b82d6db5bd800501
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-04 21:43:49 -0500

    refactor: reuse variable

--- src/hyprshade/helpers.py
+++ src/hyprshade/helpers.py
@@ -12,7 +12,5 @@ SystemdUnitType = Literal["service", "timer"]
 def write_systemd_user_unit(unit_type: SystemdUnitType, body: str):
     dest_dir = systemd_user_config_home()
     os.makedirs(dest_dir, exist_ok=True)
-    with open(
-        path.join(systemd_user_config_home(), f"hyprshade.{unit_type}"), "w"
-    ) as f:
+    with open(path.join(dest_dir, f"hyprshade.{unit_type}"), "w") as f:
         f.write(body)
