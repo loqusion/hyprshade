08abc02ea20c3247ee3f8e9bfa13d67b97366dbc
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-12-09 05:44:43 -0600

    test(current): initial test

---
+++ tests/cli/current/test_current.py
@@ -0,0 +1,34 @@
+from collections.abc import Callable
+from pathlib import Path
+
+import pytest
+from click.testing import CliRunner
+
+from hyprshade.cli import cli
+from hyprshade.shader import hyprctl
+
+pytestmark = [
+    pytest.mark.requires_hyprland(),
+    pytest.mark.usefixtures("_save_screen_shader"),
+    pytest.mark.usefixtures("_clear_shader_env"),
+]
+
+
+class TestCurrent:
+    def test_empty(self, runner: CliRunner, shader_path_env: Path):
+        hyprctl.clear_screen_shader()
+
+        result = runner.invoke(cli, ["current"])
+
+        assert result.exit_code == 0
+        assert result.output == ""
+
+    def test_it(self, runner: CliRunner, shader_path_factory: Callable[[str], Path]):
+        shader_path_factory("shader1")
+
+        hyprctl.set_screen_shader("shader1")
+
+        result = runner.invoke(cli, ["current"])
+
+        assert result.exit_code == 0
+        assert result.output.strip() == "shader1"
