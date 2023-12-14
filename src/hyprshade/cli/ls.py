92f8ed470aa2f92393e05a4f0a467301806930e5
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-12-14 14:58:29 -0600

    feat!(ls): disambiguate between different shaders with the same name

--- src/hyprshade/cli/ls.py
+++ src/hyprshade/cli/ls.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from bisect import bisect
+
 import click
 
 from hyprshade.shader import Shader
@@ -14,14 +16,25 @@ def ls(long: bool):
 
     current = Shader.current()
     shaders = list(map(Shader, ls_dirs(Shader.dirs.all())))
+    current_in_shader_paths = False
+    if current:
+        current_in_shader_paths = True
+        b = bisect(shaders, current.name, key=lambda s: s.name)
+        if shaders[b - 1] != current:
+            current_in_shader_paths = False
+            shaders.insert(b, current)
     if not shaders:
         return
     width = max(map(len, map(str, shaders))) + 1
 
     for shader in shaders:
-        c = "*" if shader == current else " "
+        is_shader_current = shader == current
+        c = "*" if is_shader_current else " "
         if long:
             dir = shader.dirname()
             click.echo(f"{c} {shader!s:{width}} {dir}")
+            continue
+        if is_shader_current and not current_in_shader_paths:
+            click.echo(f"{c} {shader!s}  ({shader.dirname()})")
             continue
         click.echo(f"{c} {shader!s}")
