8aa6e701c6befdccbe62445e8e534dde2feb9717
loqusion <38332081+loqusion@users.noreply.github.com>
2024-03-14 22:09:49 -0500

    feat(config): use new config model implementation

--- src/hyprshade/config/core.py
+++ src/hyprshade/config/core.py
@@ -3,17 +3,15 @@ from __future__ import annotations
 import os
 import tomllib
 from os import path
-from typing import cast
 
-from more_itertools import first_true, nth, partition
+from more_itertools import first_true
 
-from .types import ConfigDict, DefaultShadeConfig, ShaderConfig
+from .model import RootConfig
 from .utils import hypr_config_home, hyprshade_config_home
 
 
 class Config:
-    _config_path: str
-    _dict: ConfigDict
+    model: RootConfig
 
     def __init__(self, path_: str | None = None):
         path_ = path_ or Config._get_path()
@@ -21,40 +19,12 @@ class Config:
             raise FileNotFoundError(
                 "Could not find a config file; see https://github.com/loqusion/hyprshade#scheduling"
             )
-        self._config_path = path_
-        self._dict = Config._load(path_)
-        self._validate()
-
-    def partition(self) -> tuple[list[ShaderConfig], DefaultShadeConfig | None]:
-        no_default, yes_default = partition(
-            lambda s: s.get("default", False), self._dict["shades"]
-        )
-        rest = cast(list[ShaderConfig], list(no_default))
-        default = cast(DefaultShadeConfig, nth(yes_default, 0))
-
-        assert nth(yes_default, 0) is None, "There should be only one default shade"
-
-        return rest, default
-
-    def _validate(self) -> None:
-        shaders = self._dict.get("shades", [])
-        if not isinstance(shaders, list):
-            raise ConfigError(self._config_path, "`shades` must be a list")
-        for shader in shaders:
-            if not shader.get("name"):
-                raise ConfigError(
-                    self._config_path, "`name` is required for each item in `shades`"
-                )
-            if not shader.get("start_time") and shader.get("default") is not True:
-                raise ConfigError(
-                    self._config_path,
-                    f"Non-default shader '{shader['name']}' must define `start_time`",
-                )
+        self.model = RootConfig(Config._load(path_), path=path_)
 
     @staticmethod
-    def _load(path_: str) -> ConfigDict:
+    def _load(path_: str) -> dict:
         with open(path_, "rb") as f:
-            return cast(ConfigDict, tomllib.load(f))
+            return tomllib.load(f)
 
     @staticmethod
     def _get_path() -> str | None:
@@ -64,13 +34,3 @@ class Config:
             path.join(hyprshade_config_home(), "config.toml"),
         ]
         return first_true((c for c in candidates if c is not None), pred=path.isfile)
-
-
-class ConfigError(Exception):
-    def __init__(self, config_path: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.config_path = config_path
-
-    def __str__(self) -> str:
-        msg = super().__str__()
-        return f"Failed to parse {self.config_path}: {msg}"
--- src/hyprshade/config/schedule.py
+++ src/hyprshade/config/schedule.py
@@ -1,73 +1,75 @@
 from __future__ import annotations
 
 from itertools import chain, pairwise
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypeGuard
 
 from hyprshade.shader.core import Shader
 from hyprshade.utils import is_time_between
+
+from .model import ShaderConfig
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
     from datetime import time
 
     from .core import Config
-    from .types import ShaderConfig, TimeRange
 
-
-class ScheduleEntry:
-    shader: Shader
-    start_time: time
-    end_time: time | None
-
-    def __init__(self, shader_config: ShaderConfig):
-        self.shader = Shader(shader_config["name"])
-        self.start_time = shader_config["start_time"]
-        self.end_time = shader_config.get("end_time")
-
-    def __repr__(self) -> str:
-        return (
-            f'ScheduleEntry("{self.shader}", start_time={self.start_time}, '
-            f"end_time={self.end_time})"
-        )
+    TimeRange = tuple[time, time]
 
 
 class Schedule:
-    entries: list[ScheduleEntry]
-    default_shader: Shader | None
+    config: Config
 
     def __init__(self, config: Config):
-        rest_shader_configs, default_shader_config = config.partition()
-        sorted_rest_shader_configs = sorted(
-            rest_shader_configs, key=lambda s: s["start_time"]
-        )
-        self.entries = list(map(ScheduleEntry, sorted_rest_shader_configs))
-        self.default_shader = (
-            Shader(default_shader_config["name"])
-            if default_shader_config is not None
-            else None
-        )
+        self.config = config
 
     @classmethod
-    def from_config(cls, path_: str | None = None) -> Schedule:
+    def from_config(cls, path: str | None = None) -> Schedule:
         from .core import Config
 
-        return cls(Config(path_))
+        return cls(Config(path))
 
     def scheduled_shader(self, t: time) -> Shader | None:
-        for shader, (start_time, end_time) in self._resolved_entries():
+        for shader_name, (start_time, end_time) in self._resolved_entries():
             if is_time_between(t, start_time, end_time):
-                return shader
+                return Shader(shader_name)
 
         return self.default_shader
 
     def event_times(self) -> Iterator[time]:
-        for entry in self.entries:
+        for entry in self._entries():
             yield entry.start_time
             if entry.end_time is not None:
                 yield entry.end_time
 
-    def _resolved_entries(self) -> Iterator[tuple[Shader, TimeRange]]:
-        for entry, next_entry in pairwise(chain(self.entries, [self.entries[0]])):
+    @property
+    def default_shader(self) -> Shader | None:
+        filtered = filter(lambda s: s.default, self.config.model.shaders)
+        default = next(filtered, None)
+        assert next(filtered, None) is None
+        return Shader(default.name) if default else None
+
+    def _resolved_entries(self) -> Iterator[tuple[str, TimeRange]]:
+        if not (entries := self._entries()):
+            return iter(())
+        for entry, next_entry in pairwise(chain(entries, [entries[0]])):
+            name = entry.name
             start_time = entry.start_time
             end_time = entry.end_time or next_entry.start_time
-            yield entry.shader, (start_time, end_time)
+            yield name, (start_time, end_time)
+
+    def _entries(self) -> list[ScheduledShaderConfig]:
+        def has_schedule(
+            shader_config: ShaderConfig
+        ) -> TypeGuard[ScheduledShaderConfig]:
+            if not shader_config.default:
+                assert shader_config.start_time is not None
+                return True
+            return False
+
+        filtered = filter(has_schedule, self.config.model.shaders)
+        return sorted(filtered, key=lambda s: s.start_time)
+
+
+class ScheduledShaderConfig(ShaderConfig):
+    start_time: time
