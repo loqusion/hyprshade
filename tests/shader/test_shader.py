466646ac7d5b3a58b149f3d855aabe2c18cb457d
loqusion <38332081+loqusion@users.noreply.github.com>
2024-03-18 01:57:00 -0500

    test(refactor): move dirs tests to separate file

--- tests/shader/test_shader.py
+++ tests/shader/test_shader.py
@@ -6,29 +6,6 @@ from hyprshade.shader.core import Shader
 from tests.types import ShaderPathFactory
 
 
-class TestDirs:
-    def test_env(self, shader_dir_env: Path, monkeypatch: pytest.MonkeyPatch):
-        assert str(shader_dir_env) == Shader.dirs.env()
-        monkeypatch.delenv(Shader.dirs.ENV_VAR_NAME, raising=False)
-        assert Shader.dirs.env() != str(shader_dir_env)
-
-    def test_user_hypr(self, shader_dir_user: Path):
-        assert Shader.dirs.user_hypr() == str(shader_dir_user)
-
-    def test_system(self, shader_dir_system: Path):
-        assert str(shader_dir_system) == Shader.dirs.system()
-
-    def test_all(
-        self,
-        shader_dir_env: Path,
-        shader_dir_user: Path,
-        shader_dir_system: Path,
-    ):
-        assert Shader.dirs.all() == list(
-            map(str, [shader_dir_env, shader_dir_user, shader_dir_system])
-        )
-
-
 class TestConstructor:
     def test_name(self):
         assert Shader("foo")._name == "foo"
