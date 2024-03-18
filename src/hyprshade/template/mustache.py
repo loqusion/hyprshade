d098afc283304cf1945c3514536ee79dc2c84703
loqusion <38332081+loqusion@users.noreply.github.com>
2024-03-17 23:11:30 -0500

    refactor(template.mustache): organization

--- src/hyprshade/template/mustache.py
+++ src/hyprshade/template/mustache.py
@@ -10,36 +10,46 @@ def render(
     template: SupportsRead | str,
     data: dict[str, Any] | None = None,
 ):
-    if data is None:
-        data = {}
-
     try:
-        return chevron_render(template, data)
+        return ChevronImpl.render(template, data)
     except ImportError:
         pass
 
     try:
-        return pystache_render(template, data)
+        return PystacheImpl.render(template, data)
     except ImportError:
         pass
 
     raise ImportError("No mustache library found.")
 
 
-def chevron_render(*args, **kwargs) -> str:
-    import chevron
+class ChevronImpl:
+    @classmethod
+    def render(
+        cls,
+        template: SupportsRead[str] | str,
+        data: dict[str, Any] | None = None,
+        *args,
+        **kwargs,
+    ) -> str:
+        import chevron
 
-    return chevron.render(*args, **kwargs)
+        if data is None:
+            data = {}
+
+        return chevron.render(template, data, *args, **kwargs)
 
 
-def pystache_render(template: SupportsRead[str] | str, context=None) -> str:
-    import pystache  # type: ignore[import-not-found, import-untyped]
+class PystacheImpl:
+    @classmethod
+    def render(cls, template: SupportsRead[str] | str, context=None) -> str:
+        import pystache  # type: ignore[import-not-found, import-untyped]
 
-    template = coerce_template_to_str(template)
-    return pystache.render(template, context or {})
+        template = cls._coerce_template_to_str(template)
+        return pystache.render(template, context or {})
 
-
-def coerce_template_to_str(template: SupportsRead[str] | str) -> str:
-    if isinstance(template, str):
-        return template
-    return template.read()
+    @staticmethod
+    def _coerce_template_to_str(template: SupportsRead[str] | str) -> str:
+        if isinstance(template, str):
+            return template
+        return template.read()
