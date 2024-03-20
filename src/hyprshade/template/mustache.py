2c59c5ebecf61fe4425f3a0405f7b5e6f2850531
loqusion <38332081+loqusion@users.noreply.github.com>
2024-03-19 23:32:11 -0500

    feat(template.mustache): narrow `ImportError` suppression

--- src/hyprshade/template/mustache.py
+++ src/hyprshade/template/mustache.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from abc import ABC, abstractmethod
+from contextlib import suppress
+from typing import TYPE_CHECKING, Any, Final
 
 if TYPE_CHECKING:
     from _typeshed import SupportsRead
@@ -9,21 +11,43 @@ if TYPE_CHECKING:
 def render(
     template: SupportsRead | str,
     data: dict[str, Any] | None = None,
-):
-    try:
-        return ChevronImpl.render(template, data)
-    except ImportError:
-        pass
-
-    try:
-        return PystacheImpl.render(template, data)
-    except ImportError:
-        pass
+) -> str:
+    for impl in MustacheImpl.ALL:
+        with suppress(MustacheModuleImportError):
+            return impl.render(template, data)
 
     raise ImportError("No mustache library found.")
 
 
-class ChevronImpl:
+class MustacheModuleImportError(ImportError):
+    pass
+
+
+class MustacheImpl(ABC):
+    ALL: Final[list[type[MustacheImpl]]]  # type:ignore[misc]
+
+    @classmethod
+    @abstractmethod
+    def render(
+        cls,
+        template: SupportsRead[str] | str,
+        data: dict[str, Any] | None = None,
+        *args,
+        **kwargs,
+    ) -> str:
+        try:
+            module = cls._import_module()
+        except ImportError as e:
+            raise MustacheModuleImportError(e) from e
+
+        return module.render(template, data, *args, **kwargs)
+
+    @staticmethod
+    @abstractmethod
+    def _import_module() -> Any: ...
+
+
+class ChevronImpl(MustacheImpl):
     @classmethod
     def render(
         cls,
@@ -32,24 +56,32 @@ class ChevronImpl:
         *args,
         **kwargs,
     ) -> str:
+        return super().render(template, data or {}, *args, **kwargs)
+
+    @staticmethod
+    def _import_module():
         import chevron
 
-        if data is None:
-            data = {}
-
-        return chevron.render(template, data, *args, **kwargs)
+        return chevron
 
 
-class PystacheImpl:
+class PystacheImpl(MustacheImpl):
     @classmethod
     def render(cls, template: SupportsRead[str] | str, context=None) -> str:
+        template = cls._coerce_template_to_str(template)
+        return super().render(template, context or {})
+
+    @staticmethod
+    def _import_module():
         import pystache  # type: ignore[import-not-found, import-untyped]
 
-        template = cls._coerce_template_to_str(template)
-        return pystache.render(template, context or {})
+        return pystache
 
     @staticmethod
     def _coerce_template_to_str(template: SupportsRead[str] | str) -> str:
         if isinstance(template, str):
             return template
         return template.read()
+
+
+MustacheImpl.ALL = [ChevronImpl, PystacheImpl]  # type:ignore[misc]
