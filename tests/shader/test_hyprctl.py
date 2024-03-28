85be00c5dc54682ba34d536827a6e16f8c710770
loqusion <38332081+loqusion@users.noreply.github.com>
2024-03-27 19:19:09 -0500

    test(refactor): extract module mock to fixture

--- tests/shader/test_hyprctl.py
+++ tests/shader/test_hyprctl.py
@@ -20,16 +20,20 @@ def test_clear():
     assert hyprctl.get_screen_shader() is None
 
 
-def mock_hyprctl_invalid_json(*args, **kwargs) -> subprocess.CompletedProcess:
-    return subprocess.CompletedProcess(
-        args=args,
-        returncode=0,
-        stdout='{"str": "test}',
-        stderr="",
-    )
-
-
+@pytest.mark.usefixtures("_mock_hyprctl_invalid_json")
 def test_json_error(monkeypatch: pytest.MonkeyPatch):
-    monkeypatch.setattr(hyprctl.subprocess, "run", mock_hyprctl_invalid_json)
     with pytest.raises(RuntimeError):
         hyprctl.get_screen_shader()
+
+
+@pytest.fixture()
+def _mock_hyprctl_invalid_json(monkeypatch: pytest.MonkeyPatch):
+    def _subprocess_run_invalid_json(*args, **kwargs) -> subprocess.CompletedProcess:
+        return subprocess.CompletedProcess(
+            args=args,
+            returncode=0,
+            stdout='{"str": "test}',
+            stderr="",
+        )
+
+    monkeypatch.setattr(hyprctl.subprocess, "run", _subprocess_run_invalid_json)
