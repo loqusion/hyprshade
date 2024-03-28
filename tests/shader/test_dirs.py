55e00b7a88d3653deb070e90f521003e5f0c41fd
loqusion <38332081+loqusion@users.noreply.github.com>
2024-03-27 21:19:18 -0500

    test(shader.dirs): update tests to use `isolation` fixture

--- tests/shader/test_dirs.py
+++ tests/shader/test_dirs.py
@@ -1,28 +1,31 @@
-from pathlib import Path
-
-import pytest
-
 from hyprshade.shader.core import Shader
+from tests.conftest import Isolation
 
 
 class TestDirs:
-    def test_env(self, shader_dir_env: Path, monkeypatch: pytest.MonkeyPatch):
-        assert str(shader_dir_env) == Shader.dirs.env()
-        monkeypatch.delenv(Shader.dirs.ENV_VAR_NAME, raising=False)
-        assert Shader.dirs.env() != str(shader_dir_env)
+    def test_env(self, isolation: Isolation):
+        assert str(isolation.hyprshade_env_dir) == Shader.dirs.env()
 
-    def test_user_hypr(self, shader_dir_user: Path):
-        assert Shader.dirs.user_hypr() == str(shader_dir_user)
-
-    def test_system(self, shader_dir_system: Path):
-        assert str(shader_dir_system) == Shader.dirs.system()
-
-    def test_all(
-        self,
-        shader_dir_env: Path,
-        shader_dir_user: Path,
-        shader_dir_system: Path,
-    ):
-        assert Shader.dirs.all() == list(
-            map(str, [shader_dir_env, shader_dir_user, shader_dir_system])
+    def test_user_hypr(self, isolation: Isolation):
+        assert (
+            str(isolation.hyprshade_user_hypr_dir / "shaders")
+            == Shader.dirs.user_hypr()
         )
+
+    def test_user_hyprshade(self, isolation: Isolation):
+        assert (
+            str(isolation.hyprshade_user_hyprshade_dir / "shaders")
+            == Shader.dirs.user_hyprshade()
+        )
+
+    def test_system(self, isolation: Isolation):
+        assert str(isolation.hyprshade_system_dir / "shaders") == Shader.dirs.system()
+
+    def test_all(self, isolation: Isolation):
+        dirs = [
+            isolation.hyprshade_env_dir,
+            (isolation.hyprshade_user_hypr_dir / "shaders"),
+            (isolation.hyprshade_user_hyprshade_dir / "shaders"),
+            (isolation.hyprshade_system_dir / "shaders"),
+        ]
+        assert list(map(str, dirs)) == Shader.dirs.all()
