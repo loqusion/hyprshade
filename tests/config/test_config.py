3d471e25e8b61b8cffd8c13938a648cf30053ab5
loqusion <38332081+loqusion@users.noreply.github.com>
2024-04-12 23:12:08 -0500

    test(config): test `Config` methods

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -3,6 +3,7 @@ from datetime import time
 import pytest
 
 from hyprshade.config.core import Config
+from tests.types import ConfigFactory, ShaderPathFactory
 
 
 def test_example_config(request: pytest.FixtureRequest):
@@ -37,3 +38,172 @@ def test_example_config(request: pytest.FixtureRequest):
             },
         ]
     }
+
+
+class TestShaderConfig:
+    def test_name(self, config_factory: ConfigFactory):
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        shader_config = config.shader_config("test")
+        assert shader_config is not None
+        assert shader_config.name == "test"
+        assert shader_config.config == {"key": 3}
+
+    def test_path(
+        self, config_factory: ConfigFactory, shader_path_factory: ShaderPathFactory
+    ):
+        shader_path = shader_path_factory("test")
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        shader_config = config.shader_config(str(shader_path))
+        assert shader_config is not None
+        assert shader_config.name == "test"
+        assert shader_config.config == {"key": 3}
+
+    def test_not_found(self, config_factory: ConfigFactory):
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        shader_config = config.shader_config("not-found")
+        assert shader_config is None
+
+
+class TestShaderVariables:
+    def test_name(self, config_factory: ConfigFactory):
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        shader_variables = config.shader_variables("test")
+        assert shader_variables is not None
+        assert shader_variables == {"key": 3}
+
+    def test_path(
+        self, config_factory: ConfigFactory, shader_path_factory: ShaderPathFactory
+    ):
+        shader_path = shader_path_factory("test")
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        shader_variables = config.shader_variables(str(shader_path))
+        assert shader_variables is not None
+        assert shader_variables == {"key": 3}
+
+    def test_not_found(self, config_factory: ConfigFactory):
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        shader_config = config.shader_config("not-found")
+        assert shader_config is None
+
+
+class TestLazyShaderVariables:
+    def test_name(self, config_factory: ConfigFactory):
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        lazy_shader_variables = config.lazy_shader_variables("test")
+        shader_variables = lazy_shader_variables()
+        assert shader_variables is not None
+        assert shader_variables == {"key": 3}
+
+    def test_path(
+        self, config_factory: ConfigFactory, shader_path_factory: ShaderPathFactory
+    ):
+        shader_path = shader_path_factory("test")
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        lazy_shader_variables = config.lazy_shader_variables(str(shader_path))
+        shader_variables = lazy_shader_variables()
+        assert shader_variables is not None
+        assert shader_variables == {"key": 3}
+
+    def test_not_found(self, config_factory: ConfigFactory):
+        config_factory.write(
+            {
+                "shaders": [
+                    {
+                        "name": "test",
+                        "config": {"key": 3},
+                    }
+                ]
+            }
+        )
+        config = config_factory.get_config()
+
+        shader_config = config.shader_config("not-found")
+        assert shader_config is None
