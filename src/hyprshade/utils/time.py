ba1d48a5f393edbe7f5cd3f59f9cf6148de32677
loqusion <38332081+loqusion@users.noreply.github.com>
2024-04-12 21:18:58 -0500

    feat(is_time_between)!: `end_time` exclusive range; assert `start_time` != `end_time`

--- src/hyprshade/utils/time.py
+++ src/hyprshade/utils/time.py
@@ -7,6 +7,8 @@ if TYPE_CHECKING:
 
 
 def is_time_between(time_: time, start_time: time, end_time: time) -> bool:
-    if end_time <= start_time:
-        return start_time <= time_ or time_ <= end_time
-    return start_time <= time_ <= end_time
+    assert start_time != end_time
+
+    if end_time < start_time:
+        return start_time <= time_ or time_ < end_time
+    return start_time <= time_ < end_time
