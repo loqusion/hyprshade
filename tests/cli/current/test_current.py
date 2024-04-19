e5441017fb727acf283203d9ee9f5e4056dd6ed8
loqusion <38332081+loqusion@users.noreply.github.com>
2024-04-18 20:33:40 -0500

    test: remove unnecessary class

--- tests/cli/current/test_current.py
+++ tests/cli/current/test_current.py
@@ -10,18 +10,18 @@ pytestmark = [
 ]
 
 
-class TestCurrent:
-    def test_empty(self, runner: CliRunner):
-        hyprctl.clear_screen_shader()
-        result = runner.invoke(cli, ["current"])
+def test_empty(runner: CliRunner):
+    hyprctl.clear_screen_shader()
+    result = runner.invoke(cli, ["current"])
 
-        assert result.exit_code == 0
-        assert result.output == ""
+    assert result.exit_code == 0
+    assert result.output == ""
 
-    def test_it(self, runner: CliRunner, shader_path_factory: ShaderPathFactory):
-        shader_path = shader_path_factory("shader1")
-        hyprctl.set_screen_shader(str(shader_path))
-        result = runner.invoke(cli, ["current"])
 
-        assert result.exit_code == 0
-        assert result.output.strip() == "shader1"
+def test_it(runner: CliRunner, shader_path_factory: ShaderPathFactory):
+    shader_path = shader_path_factory("shader1")
+    hyprctl.set_screen_shader(str(shader_path))
+    result = runner.invoke(cli, ["current"])
+
+    assert result.exit_code == 0
+    assert result.output.strip() == "shader1"
