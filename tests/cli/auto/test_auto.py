999cde159bc49044e8cb0cc9fde4bbb69caa7783
loqusion <38332081+loqusion@users.noreply.github.com>
2024-04-19 13:46:19 -0500

    test(cli.auto): more thorough tests

--- tests/cli/auto/test_auto.py
+++ tests/cli/auto/test_auto.py
@@ -6,6 +6,7 @@ from click.testing import CliRunner
 
 from hyprshade.cli import cli
 from hyprshade.shader import hyprctl
+from hyprshade.shader.core import Shader
 from tests.helpers import freeze_time
 from tests.types import ConfigFactory, ShaderPathFactory
 
@@ -43,54 +44,65 @@ def test_default(
 @pytest.mark.parametrize(
     ("time_str", "expected"),
     [
-        ("12:00:00", False),
-        ("19:59:30", False),
-        ("20:00:00", True),
-        ("20:30:00", True),
-        ("20:59:30", True),
-        ("21:00:00", False),
-        ("23:00:00", False),
+        ("00:00:00", "default"),
+        ("12:00:00", "default"),
+        ("19:59:30", "default"),
+        ("20:00:00", "test1"),
+        ("20:30:00", "test1"),
+        ("20:59:30", "test1"),
+        ("21:00:00", "test2"),
+        ("22:00:00", "test2"),
+        ("22:59:30", "test2"),
+        ("23:00:00", "test3"),
+        ("23:30:00", "test3"),
+        ("23:59:30", "test3"),
     ],
 )
-@pytest.mark.parametrize("with_default", [False, True])
 def test_scheduled(
     time_str: str,
-    expected: bool,
-    with_default: bool,
+    expected: str,
     runner: CliRunner,
     shader_path_factory: ShaderPathFactory,
     config_factory: ConfigFactory,
 ):
     initial_shader_path = shader_path_factory("initial")
-    scheduled_shader_path = shader_path_factory("scheduled")
-    default_shader_path = shader_path_factory("default")
+    shader_path_factory("test1")
+    shader_path_factory("test2")
+    shader_path_factory("test3")
+    shader_path_factory("default")
     hyprctl.set_screen_shader(initial_shader_path.as_posix())
 
     config_dict = {
         "shaders": [
             {
-                "name": "scheduled",
+                "name": "test1",
                 "start_time": time.fromisoformat("20:00"),
                 "end_time": time.fromisoformat("21:00"),
             },
+            {
+                "name": "test2",
+                "start_time": time.fromisoformat("21:00"),
+            },
+            {
+                "name": "test3",
+                "start_time": time.fromisoformat("23:00"),
+                "end_time": time.fromisoformat("00:00"),
+            },
+            {
+                "name": "default",
+                "default": True,
+            },
         ]
     }
-    if with_default:
-        config_dict["shaders"].append({"name": "default", "default": True})
     config_factory.write(config_dict)
 
     with freeze_time(time_str):
         result = runner.invoke(cli, ["auto"])
 
-        if expected:
-            assert result.exit_code == 0
-            assert hyprctl.get_screen_shader() == scheduled_shader_path.as_posix()
-        elif with_default:
-            assert result.exit_code == 0
-            assert hyprctl.get_screen_shader() == default_shader_path.as_posix()
-        else:
-            assert result.exit_code == 0
-            assert hyprctl.get_screen_shader() is None
+        assert result.exit_code == 0
+        current_screen_shader_path = hyprctl.get_screen_shader()
+        assert current_screen_shader_path is not None
+        assert Shader.path_to_name(current_screen_shader_path) == expected
 
 
 def test_no_config(runner: CliRunner):
