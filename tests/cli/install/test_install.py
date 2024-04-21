cd81778152437c0549560b3290688b21132f354c
loqusion <38332081+loqusion@users.noreply.github.com>
2024-04-21 11:15:13 -0500

    test(cli.install): test multiple schedule entries

--- tests/cli/install/test_install.py
+++ tests/cli/install/test_install.py
@@ -1,20 +1,24 @@
-from configparser import ConfigParser, SectionProxy
+import re
+from collections.abc import Sequence
+from configparser import SectionProxy
+from datetime import time
 from pathlib import Path
 
 from click.testing import CliRunner
 
 from hyprshade.cli import cli
 from tests.conftest import Isolation
+from tests.helpers import SystemdUnitParser
 from tests.types import ConfigFactory
 
 
 def parse_unit(path: Path) -> dict[str, SectionProxy]:
-    unit = ConfigParser()
+    unit = SystemdUnitParser()
     unit.read_string(path.read_text())
     return dict(unit.items())
 
 
-def test_no_shaders(
+def test_no_entries(
     runner: CliRunner, isolation: Isolation, config_factory: ConfigFactory
 ):
     config_factory.write({"shaders": []})
@@ -31,11 +35,100 @@ def test_no_shaders(
     assert timer_config["Timer"].get("OnCalendar") is None
 
 
-# TODO: Write test
-def test_multiple_shaders(
+def test_one_entry(
     runner: CliRunner, isolation: Isolation, config_factory: ConfigFactory
 ):
-    pass
+    config_factory.write(
+        {"shaders": [{"name": "foo", "start_time": time.fromisoformat("12:00")}]}
+    )
+    result = runner.invoke(cli, ["install"])
+
+    assert result.exit_code == 0
+
+    assert isolation.systemd_service_path.exists()
+    service_config = parse_unit(isolation.systemd_service_path)
+    assert isinstance(service_config["Service"]["ExecStart"], str)
+
+    assert isolation.systemd_timer_path.exists()
+    timer_config = parse_unit(isolation.systemd_timer_path)
+    assert isinstance(timer_config["Timer"]["OnCalendar"], str)
+    assert re.search(r"12:00:00", timer_config["Timer"]["OnCalendar"])
+
+
+def test_two_entries(
+    runner: CliRunner, isolation: Isolation, config_factory: ConfigFactory
+):
+    config_factory.write(
+        {
+            "shaders": [
+                {
+                    "name": "foo",
+                    "start_time": time.fromisoformat("12:00"),
+                    "end_time": time.fromisoformat("13:00"),
+                }
+            ]
+        }
+    )
+    result = runner.invoke(cli, ["install"])
+
+    assert result.exit_code == 0
+
+    assert isolation.systemd_service_path.exists()
+    service_config = parse_unit(isolation.systemd_service_path)
+    assert isinstance(service_config["Service"]["ExecStart"], str)
+
+    assert isolation.systemd_timer_path.exists()
+    timer_config = parse_unit(isolation.systemd_timer_path)
+    assert isinstance(timer_config["Timer"]["OnCalendar"], Sequence)
+    assert len(timer_config["Timer"]["OnCalendar"]) == 2
+    assert re.search(r"12:00:00", timer_config["Timer"]["OnCalendar"][0])
+    assert re.search(r"13:00:00", timer_config["Timer"]["OnCalendar"][1])
+
+
+def test_multiple_entries(
+    runner: CliRunner, isolation: Isolation, config_factory: ConfigFactory
+):
+    config_factory.write(
+        {
+            "shaders": [
+                {
+                    "name": "foo",
+                    "start_time": time.fromisoformat("12:00"),
+                    "end_time": time.fromisoformat("13:00"),
+                },
+                {
+                    "name": "bar",
+                    "start_time": time.fromisoformat("13:00"),
+                    "end_time": time.fromisoformat("14:00"),
+                },
+                {
+                    "name": "baz",
+                    "start_time": time.fromisoformat("14:30"),
+                },
+                {
+                    "name": "qux",
+                    "start_time": time.fromisoformat("15:00"),
+                },
+            ]
+        }
+    )
+    result = runner.invoke(cli, ["install"])
+
+    assert result.exit_code == 0
+
+    assert isolation.systemd_service_path.exists()
+    service_config = parse_unit(isolation.systemd_service_path)
+    assert isinstance(service_config["Service"]["ExecStart"], str)
+
+    assert isolation.systemd_timer_path.exists()
+    timer_config = parse_unit(isolation.systemd_timer_path)
+    assert isinstance(timer_config["Timer"]["OnCalendar"], Sequence)
+    assert len(timer_config["Timer"]["OnCalendar"]) == 5
+    assert re.search(r"12:00:00", timer_config["Timer"]["OnCalendar"][0])
+    assert re.search(r"13:00:00", timer_config["Timer"]["OnCalendar"][1])
+    assert re.search(r"14:00:00", timer_config["Timer"]["OnCalendar"][2])
+    assert re.search(r"14:30:00", timer_config["Timer"]["OnCalendar"][3])
+    assert re.search(r"15:00:00", timer_config["Timer"]["OnCalendar"][4])
 
 
 def test_no_config(runner: CliRunner, isolation: Isolation):
