a37a072937f019987949d0c3c50b8a6d675fe7a0
loqusion <38332081+loqusion@users.noreply.github.com>
2024-04-22 11:05:21 -0500

    test(cli.toggle): `--fallback-auto` tests

--- tests/cli/toggle/test_toggle.py
+++ tests/cli/toggle/test_toggle.py
@@ -1,10 +1,13 @@
 from collections.abc import Sequence
+from datetime import time
 
 import pytest
 from click.testing import CliRunner
 
 from hyprshade.cli import cli
 from hyprshade.shader import hyprctl
+from hyprshade.shader.core import Shader
+from tests.helpers import freeze_time
 from tests.types import ConfigFactory, ShaderPathFactory
 
 pytestmark = [
@@ -84,6 +87,70 @@ def test_fallback_default(
         assert hyprctl.get_screen_shader() == str(default_shader_path)
     else:
         assert hyprctl.get_screen_shader() is None
+
+
+@pytest.mark.parametrize(
+    ("time_str", "expected"),
+    [
+        ("00:00:00", "default"),
+        ("12:00:00", "default"),
+        ("19:59:30", "default"),
+        ("20:00:00", "test1"),
+        ("20:30:00", "test1"),
+        ("20:59:30", "test1"),
+        ("21:00:00", "test2"),
+        ("22:00:00", "test2"),
+        ("22:59:30", "test2"),
+        ("23:00:00", "test3"),
+        ("23:30:00", "test3"),
+        ("23:59:30", "test3"),
+    ],
+)
+def test_fallback_auto(
+    time_str: str,
+    expected: str,
+    runner: CliRunner,
+    shader_path_factory: ShaderPathFactory,
+    config_factory: ConfigFactory,
+):
+    config_factory.write(
+        {
+            "shaders": [
+                {
+                    "name": "test1",
+                    "start_time": time.fromisoformat("20:00"),
+                    "end_time": time.fromisoformat("21:00"),
+                },
+                {
+                    "name": "test2",
+                    "start_time": time.fromisoformat("21:00"),
+                },
+                {
+                    "name": "test3",
+                    "start_time": time.fromisoformat("23:00"),
+                    "end_time": time.fromisoformat("00:00"),
+                },
+                {
+                    "name": "default",
+                    "default": True,
+                },
+            ]
+        }
+    )
+    shader_path_factory("test1")
+    shader_path_factory("test2")
+    shader_path_factory("test3")
+    shader_path_factory("default")
+    initial_shader_path = shader_path_factory("initial")
+    hyprctl.set_screen_shader(str(initial_shader_path))
+
+    with freeze_time(time_str):
+        result = runner.invoke(cli, ["toggle", "initial", "--fallback-auto"])
+
+        assert result.exit_code == 0
+        current_screen_shader_path = hyprctl.get_screen_shader()
+        assert current_screen_shader_path is not None
+        assert Shader.path_to_name(current_screen_shader_path) == expected
 
 
 def test_no_config_no_positional_argument(runner: CliRunner):
