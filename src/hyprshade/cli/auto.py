5d79ddb881f845a83939c474cf4196566ff84b3b
loqusion <38332081+loqusion@users.noreply.github.com>
2024-05-03 15:17:48 -0500

    feat(cli.auto): remove `--var` option

--- src/hyprshade/cli/auto.py
+++ src/hyprshade/cli/auto.py
@@ -5,7 +5,6 @@ from typing import TYPE_CHECKING
 
 import click
 
-from hyprshade.cli.utils import option_variables
 from hyprshade.config.schedule import Schedule
 from hyprshade.shader.core import Shader
 
@@ -14,14 +13,8 @@ if TYPE_CHECKING:
 
 
 @click.command(short_help="Set screen shader on schedule")
-@click.option(
-    "--var",
-    "-V",
-    "variables",
-    **option_variables(),
-)
 @click.pass_obj
-def auto(obj: ContextObject, variables: dict[str, str]):
+def auto(obj: ContextObject):
     """Set screen shader based on schedule.
 
     Requires a schedule to be specified in hyprshade.toml.
@@ -32,6 +25,6 @@ def auto(obj: ContextObject, variables: dict[str, str]):
     shader = Schedule(config).scheduled_shader(t)
 
     if shader:
-        shader.on(variables)
+        shader.on()
     else:
         Shader.off()
