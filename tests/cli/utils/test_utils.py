ff67a733ce870ac34273c4bc3fc2ea80ebfb8310
loqusion <38332081+loqusion@users.noreply.github.com>
2024-05-06 12:47:16 -0500

    test: `--var` value conversion

--- tests/cli/utils/test_utils.py
+++ tests/cli/utils/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from hyprshade.cli.utils import ContextObject
+from hyprshade.cli.utils import ContextObject, VarOptionPair
 from tests.types import ConfigFactory
 
 
@@ -18,3 +18,21 @@ class TestContextObject:
         assert obj.get_config(raising=False) is None
         with pytest.raises(FileNotFoundError):
             obj.get_config(raising=True)
+
+
+class TestConvertValue:
+    def test(self):
+        assert VarOptionPair.convert_value("1") == 1
+        assert VarOptionPair.convert_value("-1") == -1
+        assert VarOptionPair.convert_value("036") == 36
+
+        assert VarOptionPair.convert_value("3.5") == 3.5
+        assert VarOptionPair.convert_value("-4.93") == -4.93
+
+        assert VarOptionPair.convert_value("4.0.0") == "4.0.0"
+        assert VarOptionPair.convert_value("-4.0.0") == "-4.0.0"
+
+        assert VarOptionPair.convert_value("0x36") == "0x36"
+        assert VarOptionPair.convert_value("3b") == "3b"
+
+        assert VarOptionPair.convert_value("foo") == "foo"
